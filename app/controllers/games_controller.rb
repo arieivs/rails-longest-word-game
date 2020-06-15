c71085ecb2bbd13546e02ffc55d85dea4dc87f98
Sara Vieira <s.cvieira@sapo.pt>
2020-06-16 00:16:34 +0100

    longest word game, still need to figure out how to connect grid from new to score

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,44 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  @@score = 0
+
+  def new
+    alphabet = ('A'..'Z').to_a
+    @grid = []
+    10.times { @grid.append(alphabet[rand(0...alphabet.length)]) }
+    print @grid
+  end
+
+  def score
+    @grid = params[:grid]
+    @attempt = params[:word]
+    check_english = english_word(@attempt)
+    @result = { english?: check_english["found"], grid?: in_grid?(@attempt, @grid) }
+    @@score += check["length"] if in_grid?(@attempt, @grid) && check["found"]
+  end
+
+  def english_word(attempt)
+    url = "https://wagon-dictionary.herokuapp.com/" + attempt
+    check_serialized = open(url).read
+    JSON.parse(check_serialized)
+  end
+
+  def in_grid?(attempt, grid)
+    print "hello inside in_grid?"
+    print @grid
+    print @grid.class
+    attempt_a = attempt.upcase.split("")
+    attempt_a.each do |letter|
+      print letter
+      if grid.index(letter)
+        grid.delete_at(grid.index(letter))
+      else
+        return false
+      end
+      print grid
+    end
+    true
+  end
+end
